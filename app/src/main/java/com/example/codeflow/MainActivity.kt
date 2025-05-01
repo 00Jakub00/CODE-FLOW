d76648b1905c9d0fe5e83b2c577e15b1e4a449aa
00Jakub00 <160758193+00Jakub00@users.noreply.github.com>
2025-05-01 13:37:07 +0200

    Initial commit

--- app/src/main/java/com/example/codeflow/MainActivity.kt
+++ app/src/main/java/com/example/codeflow/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CodeFlowTheme {
+            CodeFlowTheme {git
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
